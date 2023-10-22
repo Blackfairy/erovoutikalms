e938728f90ac241e86c0f8a73fc4e15a314fc18c
unknown <amanterenz1@gmail.com>
2023-10-22 19:22:31 +0800

    new files added by renz amante & shane estrella

---
+++ includes/signup.inc.php
@@ -0,0 +1,62 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] === "POST")   {
+
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try {
+        
+        require_once 'dbh.inc.php';
+        require_once 'signup_model.inc.php';
+        //require_once 'signup_view.inc.php';
+        require_once 'signup_contr.inc.php';
+
+        //ERROR HANDLERSSSS
+        $errors = [];
+
+        if (is_input_empty($username, $pwd, $email))    {
+            $errors["empty_input"] = "Fill in all fields!";
+        }
+        if (is_email_invalid($email))    {
+            $errors["invali_email"] = "Invalid email used!";
+        }
+        if (is_username_taken($pdo, $username))    {
+            $errors["username_taken"] = "Username already taken!";
+        }
+        if (is_email_registered(object $pdo, string $email))    {
+            $errors["email_used"] = "Email already registered!";
+        }
+
+        require_once 'config_session.inc.php';
+
+        if($errors) {
+            $_SESSION["errors_signup"] = $errors;
+
+            $signupDAta = [
+                "username" => $username;
+                "email" => $email;
+            ];
+            $_SESSION["signup_data"] = $signupDAta;
+
+            header("Location: ../index.php");
+            die();
+        }
+        
+        create_user($pdo, $username, $pwd, $email);
+
+        header("Location: ../index.php?signup=success");
+        
+        $pdo = null;
+        $stmt = null;
+
+        die();
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+
+} else  {
+    header("Location: ../index.php");
+    die();
+}
---
+++ includes/signup_view.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+function check_signup_errors()  {
+    
+    if(isset($_SESSION['errors_signup']))   {
+        $error = $_SESSION['errors_signup'];
+
+        echo "<br>";
+
+        foreach ($errors as $error) {
+            echo '<p class="form-error">' . $error . '</p>';
+        }
+        unset($_SESSION['errors_signup']);
+    }
+    elseif (isset($GET["signup"]) && $GET["signup"] === "success")  {
+        echo '<br>';
+        echo '<p class="form-success">Signup success!</p>';
+    }
+}
