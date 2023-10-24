49fd000adbec70465365ad9ccfbb6096ff90a262
unknown <amanterenz1@gmail.com>
2023-10-24 14:55:48 +0800

    updated by renz

--- controllerUserData.php
+++ controllerUserData.php
@@ -98,7 +98,7 @@ if(isset($_POST['signup'])){
                 if($status == 'verified'){
                   $_SESSION['email'] = $email;
                   $_SESSION['password'] = $password;
-                    header('location: DashboardPBL.php');
+                    header('location: courses.html');
                 }else{
                     $info = "It looks like you haven't still verify your email - $email";
                     $_SESSION['info'] = $info;
